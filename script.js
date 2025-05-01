388d399476b75cc173ddcc6ad6e741a704500a7a
madisondegrezia <madisondegrezia26@gmail.com>
2025-05-01 00:23:00 -0400

    Added event listeners to update DOM

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 // Write your code here
+console.log("script connected!!!");
+const lightbulb1 = document.querySelector("#lightbulb1");
+const lightbulb2 = document.querySelector("#lightbulb2");
+const lightbulb3 = document.querySelector("#lightbulb3");
+const output = document.querySelector(".subtitle");
+let count = 0;
+
+lightbulb1.addEventListener("click", function () {
+  count += 1;
+  output.innerHTML =
+    count === 1
+      ? `You've clicked the lights ${count} time`
+      : `You've clicked the lights ${count} times`;
+  lightbulb1.classList.toggle("active");
+});
+
+lightbulb2.addEventListener("click", function () {
+  count += 1;
+  output.innerHTML =
+    count === 1
+      ? `You've clicked the lights ${count} time`
+      : `You've clicked the lights ${count} times`;
+  lightbulb2.classList.toggle("active");
+});
+
+lightbulb3.addEventListener("click", function () {
+  count += 1;
+  output.innerHTML =
+    count === 1
+      ? `You've clicked the lights ${count} time`
+      : `You've clicked the lights ${count} times`;
+  lightbulb3.classList.toggle("active");
+});
